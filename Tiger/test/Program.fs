7ebd7e7b58d64f01192f2f002e6b6c8e21d80b6a
natsutan <YRW03704@nifty.com>
2019-08-13 23:38:49 +0900

    work

---
+++ Tiger/test/Program.fs
@@ -0,0 +1,13 @@
+﻿// F# の詳細については、http://fsharp.org を参照してください
+// 詳細については、'F# チュートリアル' プロジェクトを参照してください。
+open NUnit.Framework
+
+[<Test>]
+let sum_test() =
+    Assert.AreEqual(9, 9)
+
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // 整数の終了コードを返します
